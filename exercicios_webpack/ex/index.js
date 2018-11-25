34092791ab88e9f0908c89f71f3bfb1563b50436
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-11-24 22:24:45 -0200

    Interpretando o Spread Operator do ES2015 com babel-plugin-transform-object-rest-spread

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -16,3 +16,21 @@ info('Usando o Padrao do CommonsJS!');
 
 const pessoa = new Pessoa('Wilson');
 console.log(pessoa.toString());
+
+
+//TESTE EXEMPLO usando Spread Operator do ES2015
+
+const produto = {
+      nome: 'Caneta Bic Azul',
+      preco: 1.90
+}
+
+function clone(objeto) {
+      //Com o plugin 'transform-object-rest-spread' do babel, podemos utilizar o Spread Operator
+      return { ...objeto }; // Gerando uma copia do objeto com todos os seus atributos com Spread Operator
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta Bic Preta';
+
+console.log(novoProduto, produto);
