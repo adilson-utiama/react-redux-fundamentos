e22465da326148db7585791ce7070cace09cea1c
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-04 21:20:32 -0200

    Action Limpar input da descricao do Todo

--- todo-app-redux/frontend/src/todo/todoActions.js
+++ todo-app-redux/frontend/src/todo/todoActions.js
@@ -39,9 +39,13 @@ export const search = () => {
 export const add = (description) => {
       return dispatch => {
             axios.post(URL, { description: description })
-                  .then(resp => dispatch( { type: 'TODO_ADDED', payload: resp.data } ))
+                  .then(resp => dispatch( clear() ))
                   .then(resp => dispatch( search() ))
       }
+}
+
+export const clear = () => {
+      return { type: 'TODO_CLEAR' }
 }
 
 
--- todo-app-redux/frontend/src/todo/todoForm.jsx
+++ todo-app-redux/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search, add } from './todoActions'
+import { changeDescription, search, add, clear } from './todoActions'
 
 class TodoForm extends Component {
       
@@ -18,12 +18,12 @@ class TodoForm extends Component {
             //Destructure
             //A sintaxe de atribuição via desestruturação (destructuring assignment) é uma expressão JavaScript
             // que possibilita extrair dados de arrays ou objetos em variáveis distintas.
-            const { add, search, description } = this.props
+            const { add, search, description, clear } = this.props
 
             if (e.key === 'Enter') {
-                  e.shiftKey ? search() : add()
+                  e.shiftKey ? search() : add(description)
             } else if (e.key === 'Escape') {
-                  this.props.handleClear()
+                  clear()
             }
       }
 
@@ -56,7 +56,7 @@ class TodoForm extends Component {
                               <IconButton style='info' icon='search'
                                     onClick={ () => search() }></IconButton>
                               <IconButton style='default' icon='close'
-                                    onClick={ this.props.handleClear }></IconButton>
+                                    onClick={ this.props.clear }></IconButton>
                         </Grid>
                   </div>
             
@@ -65,6 +65,6 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ( { description: state.todo.description } )
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search, add }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search, add, clear }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
