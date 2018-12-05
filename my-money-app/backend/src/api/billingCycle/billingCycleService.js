2ae5ce9dbea7006875979972503a07d954f825dd
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-05 19:59:40 -0200

    Obtendo a qtd de registros (count)

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,19 @@
 const BillingCycle = require('./billingCycle')
 
+//rotas de recursos geradas pelo node_restful 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+
+//Rota ja automaticamente mapeada em 'routes.js'
+//Retorna a quantidade de registros
+BillingCycle.route('count', (req, res, next) => {
+      BillingCycle.count(( error, value ) => {
+            if (error) {
+                  res.status(500).json({ errors: [error] })
+            } else {
+                  res.json( { value } )
+            }
+      })
+})
 
 module.exports = BillingCycle
