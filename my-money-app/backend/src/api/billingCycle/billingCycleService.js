de490d10261ba8a71de2b7c158ea4feecdf92b5c
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-05 21:52:36 -0200

    Obtendo o Sumario de Pagamentos mais Correcao (MongoDB aggregate) update mongoose

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -7,7 +7,7 @@ BillingCycle.updateOptions({ new: true, runValidators: true })
 //Rota ja automaticamente mapeada em 'routes.js'
 //Retorna a quantidade de registros
 BillingCycle.route('count', (req, res, next) => {
-      BillingCycle.count(( error, value ) => {
+      BillingCycle.countDocuments(( error, value ) => {
             if (error) {
                   res.status(500).json({ errors: [error] })
             } else {
@@ -15,5 +15,20 @@ BillingCycle.route('count', (req, res, next) => {
             }
       })
 })
+
+//Rota para retornar sumario dos pagamentos
+BillingCycle.route('summary', (req, res, next) => {
+      BillingCycle.aggregate([
+            { $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} }, 
+            { $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}} }, 
+            { $project: {_id: 0, credit: 1, debt: 1} }
+      ]).exec((error, result) => {
+          if(error) {
+              res.status(500).json({errors: [error]})
+          } else {
+              res.json(result[0] || { credit: 0, debt: 0 })
+          }
+      })
+  })
 
 module.exports = BillingCycle
