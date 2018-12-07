4bb326ba97ace484d1ca2b8c0d10e56a92b73c11
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-07 19:40:10 -0200

    Componente Dashboard - Sem uso do Redux (Exemplo)

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { Router, Route, Redirect, hashHistory } from "react-router"
 
-import Dashboard from '../dashboard/dashboard'
+import Dashboard from '../dashboard2/dashboard2'
+//import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
