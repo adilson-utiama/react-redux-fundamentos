1104095fecb172c052411b76cf5b361597218e26
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-08 23:15:35 -0200

    Exibindo dados em BillingCycleList

--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
-      const request = axios.get(` ${BASE_URL}/billingCycles `)
+      const request = axios.get(`${BASE_URL}/billingCycles`)
       return {
-            type: 'BILLLING_CYCLES_FETCHED',
+            type: 'BILLING_CYCLES_FETCHED',
             payload: request
       }
 }
