4baba14977a33049b0f176cbb47cdb3f7c993b64
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-10 14:24:16 -0200

    Componente Input para CreditList

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -3,7 +3,7 @@ import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions';
 
 import CreditList from './creditList'
@@ -19,17 +19,17 @@ class BillingCycleForm extends Component {
             return (
                   <form role='form' onSubmit={ handleSubmit }>
                         <div className='box-body'>
-                              <Field name='name' component={ labelAndInput } 
+                              <Field name='name' component={ LabelAndInput } 
                                     label='Nome' cols='12 4' placeholder='Informe o nome'
                                     readOnly={ readOnly } />
-                              <Field name='month' component={ labelAndInput } type='number'
+                              <Field name='month' component={ LabelAndInput } type='number'
                                     label='Mês' cols='12 4' placeholder='Informe o mês'
                                     readOnly={ readOnly } />
-                              <Field name='year' component={ labelAndInput } type='number'
+                              <Field name='year' component={ LabelAndInput } type='number'
                                     label='Ano' cols='12 4' placeholder='Informe o ano'
                                     readOnly={ readOnly }/>
 
-                              <CreditList cols='12 6' />
+                              <CreditList cols='12 6' readOnly={ readOnly } />
                         </div>
                         <div className='box-footer'>
                               <button type='submit' className={`btn btn-${ this.props.submitClass }`}>
