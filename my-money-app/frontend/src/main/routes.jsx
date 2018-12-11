a1e3e3c7ebb83e63ff2178e9a435fa75852b21a3
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-10 22:16:53 -0200

    Utilizando a Navegacao de rotas com React-Router (versao 2)

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,15 +1,18 @@
 import React from "react"
-import { Router, Route, Redirect, hashHistory } from "react-router"
+import { Router, Route, IndexRoute, Redirect, hashHistory } from "react-router"
 
 //Referente ao teste com Dashboard sem REDUX
 //import Dashboard from '../dashboard2/dashboard2'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
+import App from './app'
 
 export default props => (
       <Router history={ hashHistory } >
-            <Route path='/' component={ Dashboard } />
-            <Route path='/billingCycle' component={ BillingCycle } />
+            <Route path='/' component={ App }>
+                  <Route path='/billingCycle' component={ BillingCycle } />
+                  <IndexRoute component={ Dashboard } />
+            </Route>
             <Redirect from='*' to='/' />
       </Router>
 
