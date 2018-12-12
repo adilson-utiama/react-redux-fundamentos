0741b3f67baec573f4bfd12ffa0200b1732e90d8
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-12 20:01:12 -0200

    AuthService: validacao de dados no cadastro

--- my-money-app-auth/backend/src/api/user/authService.js
+++ my-money-app-auth/backend/src/api/user/authService.js
@@ -43,3 +43,49 @@ const validateToken = ( req, res, next ) => {
             return res.status(200).send( { valid: !err } )
       })
 }
+
+const sigup = ( req, res, next ) => {
+      const name = req.body.name || ''
+      const email = req.body.email || ''
+      const password = req.body.password || ''
+      const confirmPassword = req.body.confirmPassword || ''
+
+      //valida formato do email
+      if( !email.match(emailRegex) ) {
+            return res.status(400).send( { errors: ['O email informado esta invalido.'] } )
+      }
+
+      //valida formato do password
+      if( !password.match(passwordRegex) ) {
+            return res.status(400).send( { errors: ['Senha precisa ter: uma letra minuscula, uma letra maiuscula,' +
+                  ' um numero, um caracter especial(@#$%) e tamanho entre 6-20'] } )
+      }
+
+      //compara senha e confirmacao senha
+      const salt = bcrypt.genSaltSync()
+      const passwordHash = bcrypt.hashSync( password, salt )
+      if( !bcrypt.compareSync(confirmPassword, passwordHash) ) {
+            return res.status(400).send( { errors: ['Senhas nao conferem'] } )
+      }
+
+      //verifica se email ja consta no banco
+      User.findOne( { email }, ( err, user ) => {
+            if(err) {
+                  return sendErrorsFromDB( res, err )
+            } else if( user ) {
+                  return res.status(400).send( { errors: ['Usuario ja existe'] } )
+            } else {
+                  const newUser = new User( { name, email, password: passwordHash } )
+                  newUser.save( err => {
+                        if(err) {
+                              return sendErrorsFromDB( res, err )
+                        } else {
+                              login( req, res, next )
+                        }
+                  } )
+            }
+      } )
+    
+}
+
+module.exports = { login, sigup, validateToken }
