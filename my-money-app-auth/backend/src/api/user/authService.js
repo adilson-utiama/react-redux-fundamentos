dec5fa2bbf4c5d7a369dc98a287b6b754a7f0dd0
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-12 18:23:29 -0200

    AuthService: funcoes de login e validacao de token

--- my-money-app-auth/backend/src/api/user/authService.js
+++ my-money-app-auth/backend/src/api/user/authService.js
@@ -12,3 +12,34 @@ const sendErrorsFromDB = ( res, dbErrors ) => {
       _.forIn(dbErrors, error => errors.push(error.message))
       return res.status(400).json(errors)
 }
+
+const login = ( req, res, next ) => {
+      //recebendo parametros da requisicao
+      const email = req.body.email || ''
+      const password = req.body.password || ''
+
+      User.findOne( { email }, ( err, user ) => {
+            if( err ) {
+                  return sendErrorsFromDB(res, err)
+            //verifica usuario e password usando o bcryot
+            } else if( user && bcrypt.compareSync( password, user.password ) ) {
+                  //gerando token
+                  const token = jwt.sign(user, env.authSecret, { expires: '1 day' })
+                  //extrai propriedades de 'user'
+                  const { name, email } = user
+                  //retorna usuario mais o token gerado
+                  res.json( { name, email, token } )
+            } else {
+                  return res.status(400).send( { errors: ['Usuario/Senha Invalidos'] } )
+            }
+      } )
+}
+
+const validateToken = ( req, res, next ) => {
+      //recebendo token pelo corpo da requisicao
+      const token = req.body.token || ''
+      //verifica se o token é valido
+      jwt.verify(token, env.authSecret, function( err, decoded ) {
+            return res.status(200).send( { valid: !err } )
+      })
+}
