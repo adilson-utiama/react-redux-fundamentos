0e818d7ee419fb01c94af695b0c9ed2c2502b15e
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-12-15 19:27:10 -0200

    Muadanca na configuracao das rotas - Switch

--- my-money-app-auth/frontend/src/main/routes.jsx
+++ my-money-app-auth/frontend/src/main/routes.jsx
@@ -1,19 +1,16 @@
 import React from "react"
-import { Router, Route, IndexRoute, Redirect, hashHistory } from "react-router"
+import { Switch, Route, Redirect } from 'react-router'
 
-//Referente ao teste com Dashboard sem REDUX
-//import Dashboard from '../dashboard2/dashboard2'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
-import AuthOrApp from './authOrApp'
 
 export default props => (
-      <Router history={ hashHistory } >
-            <Route path='/' component={ AuthOrApp }>
-                  <Route path='/billingCycle' component={ BillingCycle } />
-                  <IndexRoute component={ Dashboard } />
-            </Route>
-            <Redirect from='*' to='/' />
-      </Router>
+      <div className='content-wrapper'>
+            <Switch>
+                  <Route exact path='/' component={Dashboard} />
+                  <Route path='/billingCycles' component={BillingCycle} />
+                  <Redirect from='*' to='/' />
+            </Switch>
+      </div>
 
 )
