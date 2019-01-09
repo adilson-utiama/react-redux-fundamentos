2d257afe7692543d1b96dae6262d58a376a63eb2
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-09 21:28:41 -0200

    [Webpack] Usando servidor de desenvolvimento do Webpack

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
             filename: 'index.js',
             path: __dirname + '/public'
       },
+      devServer: {
+            contentBase: './public',
+            port: 9000
+      },
       optimization: {
             minimizer: [
                   new UglifyJSPlugin({
