75eda511a97e0cd0c18e0319ab9c089b716aebae
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-09 12:06:22 -0200

    [Webpack] Configurando a pasta de saida

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -2,5 +2,9 @@ const webpack = require('webpack')
 
 module.exports = {
       mode: 'development',
-      entry: './src/principal.js'
+      entry: './src/principal.js',
+      output: {
+            filename: 'index.js',
+            path: __dirname + '/public'
+      }
 } 
