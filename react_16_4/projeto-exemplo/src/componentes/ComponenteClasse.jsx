a63e7a2a3fec8904e2412aabf92742ecd7f02d15
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-10 23:31:18 -0200

    [React 16.4.0] Alguns metodos para manipular o estado do componente

--- react_16_4/projeto-exemplo/src/componentes/ComponenteClasse.jsx
+++ react_16_4/projeto-exemplo/src/componentes/ComponenteClasse.jsx
@@ -7,6 +7,30 @@ export default class ComponenteClasse extends Component {
             nome: this.props.nome
       }
 
+      //E invocado imediatamente após um componente ser montado.
+      //Se você precisar carregar dados de um endpoint remoto, 
+      //este é um bom local para instanciar a solicitação de rede.
+      componentDidMount() {
+            console.log('componentDidMount invocado!')
+      }
+
+      //E invocado imediatamente antes de um componente ser desmontado e destruído. 
+      //Execute qualquer limpeza necessária nesse método, 
+      //como invalidar cronômetros, cancelar solicitações de rede
+      // ou limpar quaisquer assinaturas criadas em componentDidMount()
+      componentWillMount() {
+            console.log('componentWillMount invocado!')
+      }
+
+      //Use shouldComponentUpdate () para permitir que o React saiba 
+      //se a saída de um componente não é afetada pela alteração atual no estado ou props. 
+      //O comportamento padrão é renderizar novamente em cada alteração de estado e, 
+      //na grande maioria dos casos, você deve confiar no comportamento padrão.
+      shouldComponentUpdate(nextProps, nextState) {
+            console.log(`shouldComponentUpdate() invocado - ${nextProps.nome} : ${nextState.nome}`)
+            return true
+      }
+
       constructor(props) {
             super(props)
             //Opcao de fazer 'bind' do this
