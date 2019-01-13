2de889625f515564a11974bdd4ea7455a226ab56
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-12 23:46:06 -0200

    [Cadastro de Usuario - Frontend] Componente Main e Home

---
+++ react_16_4/cadastro-usuario/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+      <Main icon="home" title="Inicio"
+            subtitle="Projeto Cadastro de Usuarios">
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">Sistema pra exemplificar a construção de
+            um cadastro desenvolvido em React.</p>      
+      </Main>
