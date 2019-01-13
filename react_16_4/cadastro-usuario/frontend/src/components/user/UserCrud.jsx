0494d9508933ae2ea8c2391453f3ce92a3e24e90
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-13 20:35:25 -0200

    [Cadastro de Usuario -Frontend] CRUD - Edicao e Exclusao de Usuario

--- react_16_4/cadastro-usuario/frontend/src/components/user/UserCrud.jsx
+++ react_16_4/cadastro-usuario/frontend/src/components/user/UserCrud.jsx
@@ -19,6 +19,12 @@ const initialState = {
 export default class UserCrud extends Component {
 
       state = { ...initialState }
+
+      componentWillMount() {
+            axios(baseUrl).then(resp => {
+                  this.setState({ list: resp.data })
+            })
+      }
       
       clear() {
             this.setState({ user: initialState.user })
@@ -36,9 +42,9 @@ export default class UserCrud extends Component {
                   } )
       }
 
-      getUpdatedList(user) {
+      getUpdatedList(user, add = true) {
             const list = this.state.list.filter( u => u.id !== user.id)
-            list.unshift(user)
+            if(add) list.unshift(user)
             return list
       }
 
@@ -46,6 +52,57 @@ export default class UserCrud extends Component {
             const user = { ...this.state.user }
             user[event.target.name] = event.target.value
             this.setState({ user })
+      }
+
+      load(user) {
+            this.setState({ user })
+      }
+
+      remove(user) {
+            axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+                  const list = this.getUpdatedList(user, false)
+                  this.setState({ list })
+            })
+      }
+
+      renderRows() {
+            return this.state.list.map(user => {
+                  return (
+                        <tr key={ user.id }>
+                              <td>{ user.id }</td>
+                              <td>{ user.name }</td>
+                              <td>{ user.email }</td>
+                              <td>
+                                    <button className="btn btn-warning"
+                                          onClick={ () => this.load(user) }>
+                                          <i className="fa fa-pencil"></i>
+                                    </button>
+                                    <button className="btn btn-danger ml-2"
+                                          onClick={ () => this.remove(user) }>
+                                          <i className="fa fa-trash"></i>
+                                    </button>
+                              </td>
+                        </tr>
+                  )
+            })
+      }
+
+      renderTable() {
+            return (
+                  <table className="table mt-4">
+                        <thead>
+                              <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Ações</th>
+                              </tr>
+                        </thead>
+                        <tbody>
+                              { this.renderRows() }
+                        </tbody>
+                  </table>
+            )
       }
 
       renderForm() {
@@ -94,10 +151,12 @@ export default class UserCrud extends Component {
       }
 
       render() {
+            console.log(this.state.list)
 
             return (
                   <Main { ...headerProps } >
                         { this.renderForm() }
+                        { this.renderTable() }
                   </Main>
             )
       }
