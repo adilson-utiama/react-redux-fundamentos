c0c233fdcab74eda0700f921f83c6f5f6e1e51ff
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-13 16:44:17 -0200

    [Cadastro de Usuario -Frontend] Implementando Rotas (React Router)

--- react_16_4/cadastro-usuario/frontend/src/components/template/Nav.jsx
+++ react_16_4/cadastro-usuario/frontend/src/components/template/Nav.jsx
@@ -1,14 +1,16 @@
 import './Nav.css'
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default props =>
       <aside className="menu-area">
             <nav className="menu">
-                  <a href="#/">
+                  <Link to="/">
                         <i className="fa fa-home"> Inicio</i>
-                  </a>
-                  <a href="#/users">
+                  </Link>
+                  <Link to="/users">
                         <i className="fa fa-users"> Usuarios</i>
-                  </a>
+                  </Link>
             </nav>
       </aside>
--- react_16_4/cadastro-usuario/frontend/src/main/App.jsx
+++ react_16_4/cadastro-usuario/frontend/src/main/App.jsx
@@ -3,17 +3,37 @@ import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 
+import { HashRouter } from 'react-router-dom'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-//import Home from '../components/home/Home'
-import UserCrud from '../components/user/UserCrud'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
-export default props =>
-      <div className="app">
-            <Logo  />
-            <Nav />
-            {/* Testando UserCrud */}
-            <UserCrud />
-            <Footer />
-      </div>
+export default props => 
+      <HashRouter> 
+            { /* renderizacao da rota -> EX: http://localhost:3000/#/users */ }
+            <div className="app">
+                  <Logo  />
+                  <Nav />
+                  <Routes /> 
+                  <Footer />
+            </div>
+      </HashRouter>
+
+      /** Opcao usando BrowserRouter
+      
+            import { BrowserRouter } from 'react-router-dom'
+      
+            //
+
+            <BrowserRouter> -> uses the HTML5 history API -> htt://localhost:3000/users (sem #)
+                  ...
+                  <Routes />
+                  .... 
+            </BrowserRouter>
+      
+      
+      */
+
+
