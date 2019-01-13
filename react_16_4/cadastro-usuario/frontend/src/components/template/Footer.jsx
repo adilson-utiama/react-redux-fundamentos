4532d7d2192c28f840f16a72b929bf81a4c6ff82
Adilson S. Utiama <adilson.utiama@gmail.com>
2019-01-12 23:19:08 -0200

    [Cadastro de Usuario - Frontend] Componente Footer

--- react_16_4/cadastro-usuario/frontend/src/components/template/Footer.jsx
+++ react_16_4/cadastro-usuario/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
       <footer className="footer">
-            Rodape
+            <span>
+                  Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+                  <strong> Adilson</strong>
+            </span>
       </footer>
